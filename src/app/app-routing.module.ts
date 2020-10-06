531566458aa4b9099c99e0bd1b1d89b07925b677
Leonhard Gallagher <lenny.gallagher@gmail.com>
2020-10-07 00:33:22 +0200

    Initial fsmanager-frontend app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AirportComponent} from "./airport/airport.component";
+
+const routes: Routes = [
+  {path: 'airport', component: AirportComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
